a9a5991d31c390e2707d3b352ff8200c9b1691a2
Phuoc_Onix <nguyenphuoc10101993@onixdev2.com>
2019-09-12 18:06:54 +0700

    update db connection in settings.py

--- RestApiDemo/settings.py
+++ RestApiDemo/settings.py
@@ -184,7 +184,7 @@ DATABASES = {
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 AUTHENTICATION_BACKENDS = (
