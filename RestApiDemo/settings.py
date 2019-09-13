db3bad6822a4d76c6c014541aaa3a2236009534e
Phuoc_Onix <nguyenphuoc10101993@onixdev2.com>
2019-09-13 17:19:54 +0700

    fix css issue

--- RestApiDemo/settings.py
+++ RestApiDemo/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(n15ptom#$pk2q^(w#2c@#sjlv=rt580l4k6j@y!j15cab804$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'demo-restapi-v1.herokuapp.com']
 
