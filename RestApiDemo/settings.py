22c5537e823a5fa17fab2e290054618235fb01e1
Phuoc_Onix <nguyenphuoc10101993@onixdev2.com>
2019-09-13 16:16:30 +0700

    Update static files

--- RestApiDemo/settings.py
+++ RestApiDemo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(n15ptom#$pk2q^(w#2c@#sjlv=rt580l4k6j@y!j15cab804$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'demo-restapi-v1.herokuapp.com']
 
@@ -232,9 +232,10 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-if not os.path.exists(os.path.join(BASE_DIR, "static")):
-    os.makedirs(os.path.join(BASE_DIR, "static"))
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
